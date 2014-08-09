0703f05344dcc7f8b441a4f8fcfc2f21c828a68c
sirmordred <bambu_bambu@windowslive.com>
2014-08-09 10:02:48 +0300

    msm7x30:added and enabled overlay0 & ion writeback support * references: CAF and SPH-M840 board file * this commit is fixing the following commit https://github.com/sirmordred/samsung-kernel-msm7x30/commit/de93550437aff413c795ceaea981b9f3b7369bc8

--- arch/arm/mach-msm/board-ancora.c
+++ arch/arm/mach-msm/board-ancora.c
@@ -155,6 +155,13 @@ EXPORT_SYMBOL(switch_dev);
 #define MSM_FB_PRIM_BUF_SIZE	(roundup((800 * 480 * 4), 4096) * 2) /* 4bpp * 2 Pages */
 #endif
 
+#ifdef CONFIG_FB_MSM_OVERLAY0_WRITEBACK
+/* width x height x 3 bpp x 2 frame buffer */
+#define MSM_FB_OVERLAY0_WRITEBACK_SIZE roundup((800 * 480 * 3 * 2), 4096)
+#else
+#define MSM_FB_OVERLAY0_WRITEBACK_SIZE 0
+#endif
+
 #define MSM_FB_SIZE roundup(MSM_FB_PRIM_BUF_SIZE, 4096)
 
 #if defined(CONFIG_MSM_ION_MM_USE_CMA) || defined(CONFIG_MSM_PMEM_ADSP_USE_CMA)
@@ -169,6 +176,7 @@ static u64 msm_dmamask = DMA_BIT_MASK(32);
 static struct platform_device ion_dev;
 #define MSM_ION_AUDIO_SIZE		0x0200000
 #define MSM_ION_SF_SIZE			0x1C00000
+#define MSM_ION_WB_SIZE                 MSM_FB_OVERLAY0_WRITEBACK_SIZE
 
 #ifdef CONFIG_MSM_ION_MM_USE_CMA
 #define MSM_ION_MM_SIZE			0x3000000
@@ -180,7 +188,7 @@ static struct platform_device ion_dev;
 #define MSM_ION_MM_SIZE_CARVING	MSM_ION_MM_SIZE
 #endif
 
-#define MSM_ION_HEAP_NUM	4
+#define MSM_ION_HEAP_NUM	5
 #endif
 
 #define PMIC_GPIO_INT		27
@@ -4602,11 +4610,7 @@ static struct msm_panel_common_pdata mdp_pdata = {
 	.gpio = 30,
 	.mdp_max_clk = 192000000,
 	.mdp_rev = MDP_REV_40,
-#ifdef CONFIG_MSM_MULTIMEDIA_USE_ION
-        .mem_hid = BIT(ION_CP_MM_HEAP_ID),
-#else
-        .mem_hid = MEMTYPE_EBI0,
-#endif
+	.mem_hid = BIT(ION_CP_WB_HEAP_ID),
 };
 
 static struct msm_gpio lcd_panel_on_gpios[] = {
@@ -7371,6 +7375,14 @@ struct ion_platform_heap msm7x30_heaps[] = {
 			.memory_type = ION_EBI_TYPE,
 			.extra_data = (void *)&co_ion_pdata,
 		},
+                /* WB */
+                {
+                        .id     = ION_CP_WB_HEAP_ID,
+                        .type   = ION_HEAP_TYPE_CARVEOUT,
+                        .name   = ION_WB_HEAP_NAME,
+                        .memory_type = ION_EBI_TYPE,
+                        .extra_data = (void *)&co_ion_pdata,
+                },
 #endif
 };
 
@@ -7411,12 +7423,18 @@ static void __init reserve_pmem_memory(void)
 #endif
 }
 
+static void __init reserve_mdp_memory(void)
+{
+        mdp_pdata.ov0_wb_size = MSM_FB_OVERLAY0_WRITEBACK_SIZE;
+}
+
 static void __init size_ion_devices(void)
 {
 #ifdef CONFIG_MSM_MULTIMEDIA_USE_ION
 	ion_pdata.heaps[1].size = MSM_ION_MM_SIZE;
 	ion_pdata.heaps[2].size = MSM_ION_AUDIO_SIZE;
 	ion_pdata.heaps[3].size = MSM_ION_SF_SIZE;
+        ion_pdata.heaps[4].size = MSM_ION_WB_SIZE;
 #endif
 }
 
@@ -7426,7 +7444,7 @@ static void __init reserve_ion_memory(void)
 	msm7x30_reserve_table[MEMTYPE_EBI0].size += MSM_ION_MM_SIZE_CARVING;
 	msm7x30_reserve_table[MEMTYPE_EBI0].size += MSM_ION_AUDIO_SIZE;
 	msm7x30_reserve_table[MEMTYPE_EBI0].size += MSM_ION_SF_SIZE;
-	msm7x30_reserve_table[MEMTYPE_EBI0].size += 1;
+        msm7x30_reserve_table[MEMTYPE_EBI0].size += MSM_ION_WB_SIZE;
 #endif
 }
 
@@ -7434,6 +7452,7 @@ static void __init msm7x30_calculate_reserve_sizes(void)
 {
 	size_pmem_devices();
 	reserve_pmem_memory();
+	reserve_mdp_memory();
 	size_ion_devices();
 	reserve_ion_memory();
 }
